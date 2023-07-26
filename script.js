50e49f36ba4ac2e2bec9a455716dbd33dff90832
Ellen Jardon <e@ellens-mbp.lan>
2023-07-26 15:07:41 -0500

    studio plus bonus mission 1

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(astronautsData) {
+            
+            astronautsData.sort((a,b) => b.hoursInSpace - a.hoursInSpace);
+            for (let i = 0; i < astronautsData.length; i++) {
+                
+            
+            let div = document.getElementById("container");
+            let astronaut = astronautsData[i];
+            
+            div.innerHTML += `
+                <div class="astronaut">
+                <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                   <ui>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <li>Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills}</li>
+                </ui>
+            </div>
+            <img class ="avatar" src="${astronaut.picture}">
+            </div>
+
+                    `;
+            }
+        });
+    });
+
+});
